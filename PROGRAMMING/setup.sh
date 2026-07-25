fb7ce66225d2b154bab4c39c7aefe0aa8bbe3b78
Madhav P. Desai <madhav@ee.iitb.ac.in>
2026-07-25 15:18:36 +0530

    Added sample tcl scripts for programming and resetting fpga.

--- PROGRAMMING/setup.sh
+++ PROGRAMMING/setup.sh
@@ -1,13 +1,12 @@
-CWD=$(pwd)
-cd utils
-./compile.sh
-cd $CWD
-cd driver
-scons
-echo "From your toolchain docker window, run ./build_ajit_lib.sh in directory " $(pwd)
-cd $CWD
+#CWD=$(pwd)
+#cd utils
+#./compile.sh
+#cd $CWD
+#cd driver
+#scons
+#echo "From your toolchain docker window, run ./build_ajit_lib.sh in directory " $(pwd)
+#cd $CWD
 
 export AJIT_HPC_ACCEL_HOME=$(pwd)
 export AJIT_HPC_ACCEL_ENGINE_DRIVER_HOME=$(pwd)/driver
-
 export PATH=$PATH:$(pwd)/utils/bin:
